2b25abfbdae5799a7710f9ad66188b8d29b5f0f4
Gerardo Teruel <gerardogtn@gmail.com>
2016-03-11 17:53:17 -0600

    :rocket: Add task for writing circuit in json and for writing solutions in csv

--- app/src/main/java/gerardogtn/com/circuitsolver/ui/activity/MainActivity.java
+++ app/src/main/java/gerardogtn/com/circuitsolver/ui/activity/MainActivity.java
@@ -1,30 +1,49 @@
 package gerardogtn.com.circuitsolver.ui.activity;
 
+import android.content.Intent;
+import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import com.google.gson.Gson;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 import gerardogtn.com.circuitsolver.R;
 import gerardogtn.com.circuitsolver.data.model.Circuit;
+import gerardogtn.com.circuitsolver.data.model.CircuitSolverPrinter;
 import gerardogtn.com.circuitsolver.data.model.Gate;
 import gerardogtn.com.circuitsolver.data.model.Wire;
 import gerardogtn.com.circuitsolver.ui.dialog.AddCircuitComponentDialog;
+import gerardogtn.com.circuitsolver.ui.dialog.SolveCircuitOptionsDialog;
 import gerardogtn.com.circuitsolver.ui.view.CircuitView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = "MainActivity";
+
     @Bind(R.id.circuit_view)
     CircuitView mCircuitView;
+
+    private MyCircuitSolveListener mCircuitSolveListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
+        mCircuitSolveListener = new MyCircuitSolveListener();
     }
 
     @OnClick(R.id.fab_add_circuit_component)
@@ -71,7 +90,9 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void onSolveOptionSelected() {
-
+        SolveCircuitOptionsDialog dialog = new SolveCircuitOptionsDialog();
+        dialog.show(getSupportFragmentManager(), SolveCircuitOptionsDialog.TAG);
+        SolveCircuitOptionsDialog.setOnCircuitSolveListener(mCircuitSolveListener);
     }
 
     private void onImportOptionSelected() {
@@ -79,7 +100,8 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void onExportOptionSelected() {
-
+        ExportCircuitTask task = new ExportCircuitTask("circuit");
+        task.execute();
     }
 
     private void onClearOptionSelected() {
@@ -87,5 +109,89 @@ public class MainActivity extends AppCompatActivity {
         mCircuitView.invalidate();
     }
 
+    private class ExportCircuitTask extends AsyncTask<Void, Void, Void> {
+
+        private String mFilePath;
+
+        public ExportCircuitTask(String fileName) {
+            this.mFilePath = Environment.getExternalStorageDirectory() + File.separator + fileName;
+        }
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected Void doInBackground(Void... params) {
+            String circuitJson = (new Gson()).toJson(Circuit.getInstance());
+
+            try {
+                BufferedWriter writer = new BufferedWriter(new FileWriter(mFilePath));
+                writer.write(circuitJson);
+                writer.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            Intent shareIntent = new Intent(Intent.ACTION_SEND);
+            shareIntent.setType("text/json");
+            shareIntent.putExtra(Intent.EXTRA_STREAM, Uri.fromFile(new File(mFilePath)));
+            startActivity(Intent.createChooser(shareIntent, getString(R.string.action_write_solutions)));
+            super.onPostExecute(aVoid);
+        }
+
+    }
+
+    // TODO: ADD LOADING INDICATOR.
+    private class ExportCircuitSolutionTask extends AsyncTask<Void, Void, Void> {
+
+        private String mFilePath;
+
+        public ExportCircuitSolutionTask(String fileName) {
+            mFilePath = Environment.getExternalStorageDirectory() + File.separator + fileName;
+        }
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected Void doInBackground(Void... params) {
+            try {
+                FileWriter fileWriter = new FileWriter(mFilePath);
+                BufferedWriter writer = new BufferedWriter(fileWriter);
+                writer.write(CircuitSolverPrinter.getSolution());
+                writer.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            Intent shareIntent = new Intent(Intent.ACTION_SEND);
+            shareIntent.setType("text/csv");
+            shareIntent.putExtra(Intent.EXTRA_STREAM, Uri.fromFile(new File(mFilePath)));
+            startActivity(Intent.createChooser(shareIntent, getString(R.string.action_write_solutions)));
+            super.onPostExecute(aVoid);
+        }
+    }
+
+    private class MyCircuitSolveListener implements SolveCircuitOptionsDialog.OnCircuitSolveListener {
+
+        @Override
+        public void writeCircuitSolutionsToFileWithName(String name) {
+            ExportCircuitSolutionTask exportCircuitSolutionTask = new ExportCircuitSolutionTask(name);
+            exportCircuitSolutionTask.execute();
+        }
+    }
 
 }
